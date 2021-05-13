31df372c4d5428889ccb755f2f6258bd9a28f672
Miguel Silva <miguemx@Miguels-MacBook-Pro.local>
2021-05-12 23:56:22 -0500

    correccion del puerto

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require("./src/rutas/usuarios_rutas")(app);
 require("./src/rutas/funciones_rutas")(app);
 require("./src/rutas/boletos_rutas")(app);
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server running on port " + port);
 });
